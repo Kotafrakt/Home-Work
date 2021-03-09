a5d6c6526eb4042c5bf6afb0c0bd5f611439bd4f
Timur <Kotafrakt@gmail.com>
2021-03-09 09:56:45 +0300

    change name of method

--- HomeWorkLibrary/Program.cs
+++ HomeWorkLibrary/Program.cs
@@ -5,7 +5,24 @@ namespace HomeWorkLibrary
     class Program
     {
         static void Main(string[] args)
-        {        
+        {
+            int a = 324;
+            int l = 0;
+            int r = 1000;
+            int m = (l + r) / 2;
+            while (m != a)
+            {
+                if (m > a)
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m;
+                }
+                m = (l + r) / 2;
+            }
+            Console.WriteLine(m);
         }
     }
 }
